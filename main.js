ad5c42b50d63af0ff5b324c56e206e2613ad9e5a
Jordan <danipavlev@gmail.com>
2022-06-23 11:10:05 +0300

    new styles

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
 const searchButton = document.getElementById("search-button");
 const searchCountry = document.getElementById("search-country");
-
+const clearButton = document.getElementById("clear");
 
 searchButton.addEventListener("click", () => {
     let countryName = searchCountry.value;
     let result = document.getElementById("result");
     let endURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(endURL);
-    fetch(endURL).then((response) => response.json()).then((data) => {
-        // console.log(data[0]);
-        // console.log(data[0].capital[0]);
-        // console.log(data[0].flags.svg);
-        // console.log(data[0].name.common);
-        // console.log(data[0].continents[0]);
-        // console.log(Object.keys(data[0].currencies)[0]);
-        // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-        // console.log(Object.values(data[0].languages).toString().split(",").join(" + "));
-        
+    fetch(endURL).then((response) => response.json()).then((data) => {    
         result.innerHTML = `
         <img src='${data[0].flags.svg}' class="flag-img">
         <h2>${data[0].name.common}</h2>
@@ -63,3 +54,9 @@ searchButton.addEventListener("click", () => {
     
     
 });
+
+clearButton.addEventListener("click", () => {
+    result.innerHTML = " "
+    searchCountry.value = " "
+});
+
